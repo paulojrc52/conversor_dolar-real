1667e3e882f9320ad5732e08586e4cc727f47042
Paulo Junior <100033559+paulojrc52@users.noreply.github.com>
2022-10-23 23:06:34 -0300

    subindo arquivos
    Projeto criado com base no desafio 2 do curso "Formação Full Stack Javascript - do Zero ao Mercado".
    O desafio constituía em criar um conversor de Dolar para Real, consumindo uma API.

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const inputValue = document.querySelector('#value')
+const buttonCalcular = document.querySelector('#converter')
+const resultado = document.querySelector('#res')
+
+const url = 'https://economia.awesomeapi.com.br/json/last/'  
+const coins = 'USD-BRL'
+
+
+buttonCalcular.addEventListener('click', (event) => {
+    const valorDigDolar = inputValue.value
+    fetch(url + coins)
+        .then(response => {
+            return response.json()
+        })
+        .then((data) => {
+            const entries = Object.entries(data)
+            const valorEmDolar = entries[0][1].bid
+            converter(parseFloat(valorEmDolar).toFixed(3), parseFloat(valorDigDolar).toFixed(3))
+        })
+    
+})
+
+function converter(valorCotação, valorDigitado) {
+    const resFinal = valorCotação * valorDigitado
+
+    resultado.innerHTML = resFinal
+}
