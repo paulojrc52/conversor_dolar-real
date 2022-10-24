2af9013b88ad26d5b8567bf233e3c83014064cb5
Paulo Junior <100033559+paulojrc52@users.noreply.github.com>
2022-10-23 23:19:12 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ buttonCalcular.addEventListener('click', (event) => {
         .then((data) => {
             const entries = Object.entries(data)
             const valorEmDolar = entries[0][1].bid
-            converter(parseFloat(valorEmDolar).toFixed(3), parseFloat(valorDigDolar).toFixed(3))
+            converter(parseFloat(valorEmDolar).toFixed(2), parseFloat(valorDigDolar).toFixed(2))
         })
     
 })
